844b93ed3e75c76d1aa78681f50d0a1a4debf13d
Joanpe2400 <joanpe2400@gmail.com>
2020-11-11 09:44:35 +0100

    AceptaelReto

---
+++ p380_MeCaso.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class p380_MeCaso {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int gastos = 0;
+		int total = 0;
+		int suma = 0;
+		int cont = 0;
+		
+		cont = sc.nextInt();
+		
+		while (cont > 0) {
+			
+		}
+		
+		
+
+	}
+
+}
